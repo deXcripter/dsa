261cc6d971845b2a839f0a80dbef1ed74d423225
Johnpaul <indisputable.jay@gmail.com>
2023-11-19 19:47:43 +0100

    quick sort

---
+++ JS Algorithms/Sorting Algorithms/intermediate-sorting-algorithms/quickSort.js
@@ -0,0 +1,36 @@
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const pivot = (arr, start=0, end=arr.length + 1) =>{
+
+  let pivot = start;
+  let swapIndex = start;
+
+  for (let i = start + 1; i < arr.length; i++) {
+    if (arr[pivot] > arr[i]) {
+      swapIndex++;
+      swap(arr, swapIndex, i)
+    }
+  }
+  swap(arr, start, swapIndex)
+return swapIndex;
+}
+
+const quickSort = (arr, left=0, right=arr.length - 1) => {
+  if (left < right){
+  let pivotIndex = pivot(arr, left, right)
+  quickSort(arr, left, pivotIndex-1);
+  quickSort(arr, pivotIndex+1, right)}
+  return arr;
+}
+
+
+/* 
+Best time complexity: O(n log n)
+Worrst time complexity: O(n2)
+Best time complexity: O(n log n)
+
+Space complexity: O(log n)
