4fc4261bc02ec129b87f86ebfc0098822cb52b9e
Johnpaul <indisputable.jay@gmail.com>
2023-11-21 00:18:04 +0100

    readding the shift ds to the linkedlist

--- JS Algorithms/Data-Structures/singlyLinkedList.js
+++ JS Algorithms/Data-Structures/singlyLinkedList.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line max-classes-per-file
 class Node {
-  constructor(val){
+  constructor(val) {
     this.val = val;
     this.next = null;
   }
@@ -12,6 +13,8 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
+  // 1) PUSH
+
   push(val) {
     const newNode = new Node(val);
 
@@ -22,13 +25,44 @@ class SinglyLinkedList {
       this.tail.next = newNode;
       this.tail = newNode;
     }
-    this.length++    
+    this.length += 1;
+  }
+
+  // 2) POP
+
+  pop() {
+    if (!this.head) return undefined;
+    let iteration = this.head;
+    let newTail = null;
+    while (iteration.next) {
+      newTail = iteration;
+      iteration = iteration.next;
+    }
+    this.tail = newTail;
+    if (this.length > 1) {
+      this.tail.next = null;
+    }
+    if (!this.length) return undefined;
+    this.length -= 1;
+    return iteration;
+  }
+
+  // SHIFT
+
+  shift() {
+    if (!this.head) return undefined;
+    const oldHead = this.head;
+    this.head = oldHead.next;
+    this.length -= 1;
+
+    if (this.length === 0) {
+      this.tail = null;
+    }
+
+    return oldHead;
   }
 }
 
-const list = new SinglyLinkedList()
+const list = new SinglyLinkedList();
 
-list.push('hello')
-list.push('hi')
-list.push('hiii')
-console.log(list)
+list.push(20);
