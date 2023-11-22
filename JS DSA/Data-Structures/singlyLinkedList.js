d9940dc1fba0cf8ba9ef6e81900ca8b6e7b73e25
Johnpaul <indisputable.jay@gmail.com>
2023-11-22 02:01:20 +0100

    discovered bug in the remove method

---
+++ JS DSA/Data-Structures/singlyLinkedList.js
@@ -0,0 +1,137 @@
+// eslint-disable-next-line max-classes-per-file
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // PUSH METHOD
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
+
+  // POP METHOD
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  // SHIFT METHOD
+
+  shift() {
+    if (!this.head) return undefined;
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  // UNSHIFT METHOD
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length += 1;
+    return this;
+  }
+
+  // GET METHOD
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter += 1;
+    }
+    return current;
+  }
+
+  // SET METHOD
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  // INSERT METHOD
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    const newNode = new Node(val);
+    const prev = this.get(index - 1);
+    const temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length += 1;
+    return true;
+  }
+
+  // remove(index) {
+  //   if (index < 0 || index > this.length) return undefined;
+  //   if (index === 0) this.shift(index);
+  //   if (index === this.length - 1) this.pop(index);
+  //   const previousNode = this.get(index - 1);
+  //   const removed = previousNode.next;
+  //   previousNode.next = removed.next;
+  //   this.length -= 1;
+  //   return removed;
+  // }
+}
+
+const list = new SinglyLinkedList();
+
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+
+console.log(list.insert(0, 100), list);
