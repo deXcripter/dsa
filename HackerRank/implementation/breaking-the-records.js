8414c61bec5aee1a11cc426b2bbc99be44c19094
Johnpaul <indisputable.jay@gmail.com>
2023-12-08 20:18:36 +0100

    eslinter

--- HackerRank/implementation/breaking-the-records.js
+++ HackerRank/implementation/breaking-the-records.js
@@ -1,17 +1,18 @@
 // link => https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
 
+// eslint-disable-next-line no-unused-vars
 function breakingRecords(scores) {
   let min = 0;
   let max = 0;
   let minimum = scores[0];
   let maximum = scores[0];
 
-  for (let i = 1; i < scores.length; i++) {
+  for (let i = 1; i < scores.length; i += 1) {
     if (scores[i] > maximum) {
-      max++;
+      max += 1;
       maximum = scores[i];
     } else if (scores[i] < minimum) {
-      min++;
+      min += 1;
       minimum = scores[i];
     }
   }
