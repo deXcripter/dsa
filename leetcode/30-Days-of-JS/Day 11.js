adb9d30e1f49ddfd5a70c2fa56d9c22f027ab73e
Johnpaul <indisputable.jay@gmail.com>
2024-04-04 04:04:13 +0100

    memoise gotten

--- leetcode/30-Days-of-JS/Day 11.js
+++ leetcode/30-Days-of-JS/Day 11.js
@@ -1,27 +1,25 @@
+"use strict";
 function memoize(fn) {
-    var cache = new Map();
-    var increment = 0;
-    return function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        var key = JSON.stringify(args);
-        if (cache.get(key)) {
+    const cache = new Map();
+    let increment = 0;
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (args.length < 1)
+            return;
+        if (cache.get(key) !== undefined) {
             return cache.get(key);
         }
-        cache.set(key, fn.apply(void 0, args));
-        console.log(key);
+        cache.set(key, fn(...args));
         increment++;
         return cache.get(key);
     };
 }
-var callCount = 0;
-var memoizedFn = memoize(function (a, b) {
-    callCount += 1;
-    return a + b;
-});
-memoizedFn(0, 0); // 5
-memoizedFn(0, 0); // 5
-memoizedFn(); // 5
-console.log(callCount); // 1
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//   callCount += 1;
+//   return a + b;
+// });
+// memoizedFn(0, 0); // 5
+// memoizedFn(0, 0); // 5
+// memoizedFn(); // 5
+// console.log(callCount); // 1
--- leetcode/30-Days-of-JS/Day 11.ts
+++ leetcode/30-Days-of-JS/Day 11.ts
@@ -6,23 +6,24 @@ function memoize(fn: Fn): Fn {
 
   return function (...args) {
     const key = JSON.stringify(args);
-    if (cache.get(key)) {
+
+    if (args.length < 1) return;
+
+    if (cache.get(key) !== undefined) {
       return cache.get(key);
     }
-
     cache.set(key, fn(...args));
-    console.log(key);
     increment++;
     return cache.get(key);
   };
 }
 
-let callCount = 0;
-const memoizedFn = memoize(function (a, b) {
-  callCount += 1;
-  return a + b;
-});
-memoizedFn(0, 0); // 5
-memoizedFn(0, 0); // 5
-memoizedFn(); // 5
-console.log(callCount); // 1
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//   callCount += 1;
+//   return a + b;
+// });
+// memoizedFn(0, 0); // 5
+// memoizedFn(0, 0); // 5
+// memoizedFn(); // 5
+// console.log(callCount); // 1
