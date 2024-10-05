f3670484cf7af89e087d900e1fe09766ec0e0c06
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-05 21:52:16 +0100

    binary search

--- algorithms/search/binary-search.ts
+++ algorithms/search/binary-search.ts
@@ -1,10 +1,19 @@
-// [1,2,3,4,5,6,7,8] 8
+/**
+ * @param arr [1, 2, 3, 4, 5, 6, 7, 8, 9, 12]
+ * @param val 12
+ * @returns 9
+ *
+ * time complexity -> O(log(n))
+ */
+
 const binarySearch = (arr: number[], val: number): number => {
   let left = 0;
   let right = arr.length - 1;
   let middle = Math.floor((right + left) / 2);
 
-  while (arr[middle] !== val && left < right) {
+  console.log("Works only on sorted arrays");
+
+  while (arr[middle] !== val && left <= right) {
     if (arr[middle] < val) left = middle + 1;
     if (arr[middle] > val) right = middle - 1;
     middle = Math.floor((right + left) / 2);
