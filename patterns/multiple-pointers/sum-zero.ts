e256ecc5f8e71589ec126e00ce89547db29ae4d4
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-05 04:39:29 +0100

    multiple pointers

---
+++ patterns/multiple-pointers/sum-zero.ts
@@ -0,0 +1,14 @@
+const sum = (arr: number[]): number[] => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) return [arr[left], arr[right]];
+    else if (sum > 0) right--;
+    else left++;
+  }
+  return [];
+};
+
+console.log(sum([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
