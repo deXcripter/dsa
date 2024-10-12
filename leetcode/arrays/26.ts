5716fa3f7a9f2a30055b31ad05b31103f3807d80
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-10-12 17:18:25 +0100

    removing duplocates from sorted array

---
+++ leetcode/arrays/26.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param nums [1,1,2]
+ * @returns [1,2]
+ */
+
+function removeDuplicates(nums: number[]): number {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+}
